8f4df6fccc207f5ad7c9fcccc12d26a55782f761
Chip Wasson <chip@wasson.io>
2018-07-21 19:49:40 -0600

    Fix status bug

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    for_sale: process.env.REACT_APP_FOR_SALE ? process.env.REACT_APP_FOR_SALE : false,
-    in_use: process.env.REACT_APP_IN_USE ? process.env.REACT_APP_IN_USE : false,
+    for_sale: process.env.REACT_APP_FOR_SALE ? process.env.REACT_APP_FOR_SALE === "true" : false,
+    in_use: process.env.REACT_APP_IN_USE ? process.env.REACT_APP_IN_USE === "true"  : false,
     domain: process.env.REACT_APP_DOMAIN ? process.env.REACT_APP_DOMAIN : 'This domain',
     contact_email: process.env.REACT_APP_CONTACT_EMAIL ? process.env.REACT_APP_CONTACT_EMAIL : 'user@example.com'
 }
